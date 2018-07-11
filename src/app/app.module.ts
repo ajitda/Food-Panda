b7fa5634dadc39d429877c6c398618a65ee05a1a
ajitdas <ajitdas2900@gmail.com>
2018-07-11 23:37:03 +0600

    bootstrap nav added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { HomeComponent } from './home/home.component';
     ServerComponent,
     ServersComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AddComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
